8e0b6d3c9bde1f320fd1eb67aa5226238126ffb9
sahil <sahilsingh180799@gmail.com>
2024-08-19 00:12:42 +0530

    added server part of the user management

---
+++ User-Management-Server/src/main/java/com/user_management_server/UserManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.user_management_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementServerApplication.class, args);
+	}
+
+}
---
+++ User-Management-Server/src/test/java/com/user_management_server/UserManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_management_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

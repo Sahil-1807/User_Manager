b6bd1b69f57fbfa5e418dceb5155cf303fa4b8dd
sahil <sahilsingh180799@gmail.com>
2024-08-29 00:19:50 +0530

    added security config class to customize the authentication and authorization part

---
+++ User-Management-Server/src/main/java/com/user_management_server/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.user_management_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
